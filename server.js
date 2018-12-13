ef083914bcecac03b2c79ef15f0ebca722c84b7f
hamish <hammie217@gmail.com>
2018-12-13 21:25:37 +0000

    hi

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const port = 3000
 
 app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('index');
 });
 
 app.listen(port, () => {
